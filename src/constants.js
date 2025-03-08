d71945b3dd154c47efc72a74a411785f1185aff2
AmanDevani <amandevani25@gmail.com>
2025-03-08 22:25:10 +0530

    intial files added & some html editor added

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const EDITORS = {
+  TIPTAP: "TIPTAP",
+  QUIL: "QUIL",
+  TINYMCE: "TINYMCE",
+};
+
+export const EDITORS_OPTIONS = [
+  {
+    label: "Tip Tap (Free)",
+    value: EDITORS.TIPTAP,
+  },
+  {
+    label: "Tiny MCE (Paid)",
+    value: EDITORS.TINYMCE,
+  },
+  {
+    label: "Quil (Free)",
+    value: EDITORS.QUIL,
+  },
+];
